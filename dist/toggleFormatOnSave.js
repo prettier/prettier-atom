2cdd48914c4cdbb30bc61a0dc3ece16128d40314
Rob Wise <robert.wise@outlook.com>
2017-05-03 02:01:19 -0400

    chore(dist): compile stale dist files

--- dist/toggleFormatOnSave.js
+++ dist/toggleFormatOnSave.js
@@ -4,9 +4,10 @@ var _require = require('./helpers'),
     getConfigOption = _require.getConfigOption,
     setConfigOption = _require.setConfigOption;
 
+var FORMAT_ON_SAVE = 'formatOnSaveOptions.enabled';
+
 var toggleFormatOnSave = function toggleFormatOnSave() {
-  var key = 'formatOnSaveOptions.enabled';
-  setConfigOption(key, !getConfigOption(key));
+  setConfigOption(FORMAT_ON_SAVE, !getConfigOption(FORMAT_ON_SAVE));
 };
 
 module.exports = toggleFormatOnSave;
