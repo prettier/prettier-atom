372524c03dbb22eb9dc429a800f2cee85af776d7
Rob Wise <robert.wise@outlook.com>
2017-05-23 00:23:59 -0400

    refactor(display-debug-info): use read-pkg-up instead of read-pkg

--- src/displayDebugInfo/index.test.js
+++ src/displayDebugInfo/index.test.js
@@ -1,7 +1,7 @@
-jest.mock('read-pkg');
+jest.mock('read-pkg-up');
 jest.mock('../atomInterface');
 
-const readPkg = require('read-pkg');
+const readPkgUp = require('read-pkg-up');
 const displayDebugInfo = require('./index');
 const { getAtomVersion, getPrettierAtomConfig, addInfoNotification } = require('../atomInterface');
 
@@ -12,7 +12,7 @@ test('it displays a notification on Atom with package information', () => {
     title = _title;
     options = _options;
   });
-  readPkg.sync.mockImplementation(() => ({ version: 'FAKE_PACKAGE_VERSION' }));
+  readPkgUp.sync.mockImplementation(() => ({ version: 'FAKE_PACKAGE_VERSION' }));
   getAtomVersion.mockImplementation(() => 'FAKE_ATOM_VERSION');
   getPrettierAtomConfig.mockImplementation(() => 'FAKE_PRETTIER_ATOM_CONFIG');
 
