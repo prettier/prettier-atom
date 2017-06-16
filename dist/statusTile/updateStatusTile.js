afd289eb1e63627c5e02de15f35ea342f0374ac1
Dara Hak <dara.hak@gmail.com>
2017-06-16 17:01:48 +0200

    refactor(statusTile): Tooltip on status bar tile now indicates the user can click on it to toggle `F

--- dist/statusTile/updateStatusTile.js
+++ dist/statusTile/updateStatusTile.js
@@ -14,7 +14,9 @@ var updateStatusTile = function updateStatusTile(disposable, element) {
 
   element.dataset.prettierFormatOnSave = getFormatOnSaveStatus(); // eslint-disable-line no-param-reassign
 
-  var newTooltip = addTooltip(element, { title: 'Format on Save: ' + getFormatOnSaveStatus() });
+  var newTooltip = addTooltip(element, {
+    title: 'Format on Save: ' + getFormatOnSaveStatus() + '<br>Click to toggle'
+  });
 
   setTooltip(newTooltip);
   disposable.add(newTooltip);
--- src/statusTile/updateStatusTile.js
+++ src/statusTile/updateStatusTile.js
@@ -8,7 +8,9 @@ const updateStatusTile = (disposable: Atom$Disposable, element: HTMLElement) => 
 
   element.dataset.prettierFormatOnSave = getFormatOnSaveStatus(); // eslint-disable-line no-param-reassign
 
-  const newTooltip = addTooltip(element, { title: `Format on Save: ${getFormatOnSaveStatus()}` });
+  const newTooltip = addTooltip(element, {
+    title: `Format on Save: ${getFormatOnSaveStatus()}<br>Click to toggle`,
+  });
 
   setTooltip(newTooltip);
   disposable.add(newTooltip);
