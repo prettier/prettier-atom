9eb30642f4a46a5e14128afbad461b1ecb3cc3ce
Dara Hak <dara.hak@gmail.com>
2017-06-19 11:29:02 +0200

    refactor(statusTile): Remove guard clause for mandatory arg

--- dist/statusTile/updateStatusTileScope.js
+++ dist/statusTile/updateStatusTileScope.js
@@ -7,8 +7,6 @@ var _require2 = require('../atomInterface'),
     getAllScopes = _require2.getAllScopes;
 
 var updateStatusTileScope = function updateStatusTileScope(element, editor) {
-  if (!element) return;
-
   // The editor can be undefined if there is no active editor (e.g. closed all tabs).
   // eslint-disable-next-line no-param-reassign
   element.dataset.prettierMatchScope = editor && getAllScopes().includes(getCurrentScope(editor)) ? 'true' : 'false';
--- src/statusTile/updateStatusTileScope.js
+++ src/statusTile/updateStatusTileScope.js
@@ -3,8 +3,6 @@ const { getCurrentScope } = require('../editorInterface');
 const { getAllScopes } = require('../atomInterface');
 
 const updateStatusTileScope = (element: HTMLElement, editor: ?TextEditor) => {
-  if (!element) return;
-
   // The editor can be undefined if there is no active editor (e.g. closed all tabs).
   // eslint-disable-next-line no-param-reassign
   element.dataset.prettierMatchScope = editor && getAllScopes().includes(getCurrentScope(editor))
