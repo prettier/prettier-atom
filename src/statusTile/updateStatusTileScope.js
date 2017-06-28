3519fabe1763f80c2806632697112d7083518111
Matthieu Lemoine <mlemoine@yumii.fr>
2017-06-28 14:41:16 +0200

    feat(prettier): bump to 1.5.2

--- src/statusTile/updateStatusTileScope.js
+++ src/statusTile/updateStatusTileScope.js
@@ -5,9 +5,8 @@ const { getAllScopes } = require('../atomInterface');
 const updateStatusTileScope = (element: HTMLElement, editor: ?TextEditor) => {
   // The editor can be undefined if there is no active editor (e.g. closed all tabs).
   // eslint-disable-next-line no-param-reassign
-  element.dataset.prettierMatchScope = editor && getAllScopes().includes(getCurrentScope(editor))
-    ? 'true'
-    : 'false';
+  element.dataset.prettierMatchScope =
+    editor && getAllScopes().includes(getCurrentScope(editor)) ? 'true' : 'false';
 };
 
 module.exports = updateStatusTileScope;
